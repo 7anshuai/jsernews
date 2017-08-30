c258bb6c48aaac0bbdc307768245a6208c37301b
7anshuai <7anshuai@gmail.com>
2017-08-30 16:27:48 +0800

    Using combined log output format for production

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const app = express();
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger(app.get('env') === 'production' ? 'combined' : 'dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
